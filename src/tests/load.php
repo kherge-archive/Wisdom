bbcccd04266ae8c811999cd1da798aac2f6e5fd6
Kevin Herrera <me@kevingh.com>
2012-08-20 15:02:55 -0700

    Moving vendors to src directory.

--- src/tests/load.php
+++ src/tests/load.php
@@ -1,7 +1,7 @@
 <?php
 
 call_user_func(function () {
-    $loader = require __DIR__ . '/../../vendor/autoload.php';
+    $loader = require __DIR__ . '/../vendors/autoload.php';
 
     $loader->add(null, __DIR__);
 });
